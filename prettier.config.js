b55f404ea82c497233ea0fbbe861585abd2f59b5
sebetc4 <sebastienetcheto@gmail.com>
2024-10-19 21:02:30 +0200

    core:remove unused plugins

--- prettier.config.js
+++ prettier.config.js
@@ -1,20 +1,17 @@
 /** @type {import("prettier").Config} */
 export default {
-    plugins: ['@trivago/prettier-plugin-sort-imports', 'prettier-plugin-glsl'],
+    plugins: ['@trivago/prettier-plugin-sort-imports'],
     printWidth: 100,
     tabWidth: 4,
     semi: false,
     singleQuote: true,
-    jsxSingleQuote: true,
     bracketSameLine: false,
     singleAttributePerLine: true,
     importOrder: [
         '.*\\.(css|scss)$',
-        '^react$',
         '^@?\\w',
-        '^(?!@/types)(@/|.)(?!.*.glsl$)',
+        '^(?!@/types)(@/|.)',
         '^@/types',
-        '.*\\.glsl$',
     ],
     importOrderSeparation: true,
     importOrderSortSpecifiers: true,
